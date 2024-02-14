929fcb0c0b1d69829b55408e8d865bb3c9e4f50c
Ohioooooo <deruvalcaba@miners.utep.edu>
2024-02-14 11:59:22 -0700

    changes

--- code_4.py
+++ code_4.py
@@ -1,30 +1,27 @@
 import csv
 import pandas as pd
-
-big_mac_file = './big-mac-full-index.csv'
-data_frame = pd.read_csv(big_mac_file)
-country_code = "MEX"
+df = pd.read_csv('big-mac-full-index.csv')
 
 def get_big_mac_price_by_year(year, country_code): 
-    query_str = "(iso_a3 == '{country_code}') & (data_frame['date'].dtyear == {year})"
-    mex_df = data_frame.query(query_str)
-    return round(mex_df['dollar_price'].mean(), 2)
+    results = []
+    for i,r in df.iterrows():
+        year_str = str(r['date'])
+        country_code_lower = str(r['iso_a3']).lower()
+        mean_price = round (float(r['dollar_price']),2)
+        print(year_str, country_code_lower, mean_price)
+    
+#def get_big_mac_price_by_country(country_code):
+    #query_str = f"(iso_a3 == '{country_code.lower})"
+    #mex_df = df.search(query_str)
+    #return round(mex_df['dollar_price'].mean(),2)
 
-year = 2022
-print(get_big_mac_price_by_year(year, country_code))
-
-def get_big_mac_price_by_country(country_code):
-    query_str = f"(iso_a3 == '{country_code.lower})"
-    mex_df = df.search(query_str)
-    return round(mex_df['dollar_price'].mean(),2)
-
-def get_the_cheapest_big_mac_price_by_year(year):
-    pass # Remove this line and code your function
+#def get_the_cheapest_big_mac_price_by_year(year):
+    #pass # Remove this line and code your function
 
 #def get_the_most_expensive_big_mac_price_by_year(year):
     #pass # Remove this line and code your function
 
 if __name__ == "__main__":
-    year = 2022 
+    year = 'date'
+    country_code = 'iso_a3'
     print(get_big_mac_price_by_year(year, country_code))
-    
